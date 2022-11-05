eda822e581d09049364076ea7a6bc410fdb8d66b
HarizDharma <jingh3191@gmail.com>
2022-11-06 04:25:00 +0700

    initial commit

---
+++ Karyawan.js
@@ -0,0 +1,27 @@
+const karyawan = [
+    {id:'1', nama:'Kharisma Dharma Putra', jabatan:'Direktur',
+    email: 'kharis@gmail.com', telp:'081212344312',
+    gaji:'100.000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:'2', nama:'Figur Panca Pramana', jabatan:'Web Developer',
+    email:'figur@gmail.com', telp:'087899004556',
+    gaji:'90.000.000', foto:'https://placeimg.com/100/100/arch'},
+
+    {id:'3', nama:'Fajar Herlambang', jabatan:'Designer',
+    email:'pajar@gmail.com', telp:'088556677889',
+    gaji:'80.000.000', foto:'https://placeimg.com/100/100/people'},
+
+    {id:'4', nama:'Vidya Meidita', jabatan:'System Developer',
+    email:'vidya@gmail.com', telp:'087356453487',
+    gaji:'80.000.000', foto:'https://placeimg.com/100/100/tech'},
+
+    {id:'5', nama:'Avril Lavigne', jabatan:'Content Designer',
+    email:'avril@gmail.com', telp:'081245560987',
+    gaji:'60.000.000', foto:'https://placeimg.com/100/100/nature'},
+
+    {id:'6', nama:'Ivan Toyes', jabatan:'Database Administrator',
+    email:'ivan@gmail.com', telp:'0821000088000',
+    gaji:'70.000.000', foto:'https://placeimg.com/100/100/tech'}
+];
+
+export default karyawan;
